4377e83cfe4fe720c50ec753ae117a5d49b3dd65
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-07 17:24:14 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css';
 import React  from 'react';
 import Blog from './pages/Blog';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import { Route } from 'react-router-dom';
 
-const App = ( ) => {
-  return (
-    <div>
-       <Route path='/' component={Home} />
-       <Route path='/blog/:id' component={Blog} />
+const App = ( ) => (
+      <div className='container'>
+      <Switch>
+     <Route path="/" component={Home} />
+    <Route path="/blog/:id" component={Blog} />
+  </Switch>
     </div>
   );
-}
 
 export default App;
+
