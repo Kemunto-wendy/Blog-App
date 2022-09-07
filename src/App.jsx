7bd6422369294a075f93d3b345353f4dc4a77353
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-07 18:06:43 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import { Routes, Route } from 'react-router-dom';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
 
-
 const App = ( ) => (
       <div className='container'>
       <Routes>
-   <Route path="/"component={Home} />
-   <Route path='/blog/:id'component={Blog} />
-
+   <Route path="/"element={<Home />} />
+   <Route path="/blog/:id"element={<Blog />} />
   </Routes>
     </div>
   );
