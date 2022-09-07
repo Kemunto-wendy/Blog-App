1657a5221db5934630cbbf542329e004b21593bc
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-07 17:29:24 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import React  from 'react';
 import Blog from './pages/Blog';
-import Home from './pages/Home';
-import { Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
 
 const App = ( ) => (
       <div className='container'>
-      <Switch>
+      <Routes>
      <Route path="/" component={Home} />
     <Route path="/blog/:id" component={Blog} />
-  </Switch>
+  </Routes>
     </div>
   );
 
