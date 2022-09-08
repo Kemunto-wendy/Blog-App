fa57df070f4801800639391008e189287c070792
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-08 18:35:30 +0300

    changes

--- src/components/Home/Blogging/blogging.jsx
+++ src/components/Home/Blogging/blogging.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Blogitem from "./Blogitem/item";
 import "./blogging.css";
 
-const Blogging = ({ blogs }) => {
-    return (
-      <div className='blogList-wrap'>
-        {blogs.map((blog) => (
-          <Blogitem blog={blog} />
+const Blogging = ({ blogs }) => (
+        <div className='blogList-wrap'>
+              {blogs.map(( blog ) => (
+                  <Blogitem blog={blog}
+                  key={blog.id}
+                  />
         ))}
       </div>
     );
-  };
+
 
 export default Blogging;
