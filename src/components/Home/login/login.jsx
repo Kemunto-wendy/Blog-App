e5dcaf8e53dd811fb7550ace903d09b11bc2d2ed
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-10 11:38:16 +0300

    changes

---
+++ src/components/Home/login/login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+    return (
+        <header className="login">
+        <h2 class="logo">Portfo<span>lio</span></h2>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT ME</a></li>
+            <li><a href="#">SKILLS</a></li>
+            <li><a href="#">CONTACT ME</a></li>
+        </ul>
+        </header>
+    );
+}
+
+
+export default Login
